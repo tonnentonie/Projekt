8795ef9c35c36daaeaf087be9dc91ca47605a9a6
Mika Bausch <m.bausch@ostfalia.de>
2023-05-05 19:04:19 +0200

    init

---
+++ Pollack/src/components/CreatePoll.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id='card' >
+      <p>  {{ str }}</p> 
+  
+    </div>
+  
+  
+  </template>
+  
+  
+  <script setup>
+    let str = "Hello j!";
+  
+    </script>
---
+++ Pollack/src/components/PollCard.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div id='card' >
+    <p>  {{ str }}</p> 
+
+  </div>
+
+
+</template>
+
+
+<script setup>
+  let str = "Hello j!";
+
+  </script>
+
+
+
+<style scoped>
+  #card{
+    background-color: rgb(23, 109, 185);
+    
+  }
+</style>
